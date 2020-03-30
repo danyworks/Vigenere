7fb98b9fb214550bfdc0defda3b7ae2516508eeb
Daniyal Ibrahim <48337074+daniyalIbrahim@users.noreply.github.com>
2020-03-30 21:26:44 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//sample text input
+	msg := "THis is the normal text"
+	//sample key
+  key := "dog"
+  
+  // encrypt
+	encipher := Encipher(msg,key)
+	
+	fmt.Println(encipher)
+  
+	// decrypt
+  decrypt := Decipher(encipher, key)
+	fmt.Println(decrypt)
+	
+}
+
+func Sanitize(in string) string {
+	out := []rune{}
+	for _, v := range in {
+		if 65 <= v && v <= 90 {
+			out = append(out, v)
+		} else if 97 <= v && v <= 122 {
+			out = append(out, v-32)
+		}
+	}
+
+	return string(out)
+}
+
+func Quartets(in string) string {
+	out := make([]rune, 0, len(in))
+	for i, v := range in {
+		if i%4 == 0 && i != 0 {
+			out = append(out, rune(32))
+		}
+		out = append(out, v)
+	}
+	return string(out)
+}
+
+func EncodePair(a, b rune) rune {
+	return (((a - 'A') + (b - 'A')) % 26) + 'A'
+}
+
+func DecodePair(a, b rune) rune {
+	return (((((a - 'A') - (b - 'A')) + 26) % 26) + 'A')
+}
+
+func Encipher(msg, key string) string {
+	smsg, skey := Sanitize(msg), Sanitize(key)
+	out := make([]rune, 0, len(msg))
+	for i, v := range smsg {
+		out = append(out, EncodePair(v, rune(skey[i%len(skey)])))
+	}
+	return string(out)
+}
+
+func Decipher(msg, key string) string {
+	smsg, skey := Sanitize(msg), Sanitize(key)
+	out := make([]rune, 0, len(msg))
+	for i, v := range smsg {
+		out = append(out, DecodePair(v, rune(skey[i%len(skey)])))
+	}
+	return string(out)
